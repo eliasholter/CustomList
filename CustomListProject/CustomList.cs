27584f8862cc20d9f85378115ad8d8727a47f007
Elias Holter <eliasholter1@gmail.com>
2019-12-05 16:28:52 -0600

    Added unit tests for adding item to list and for adding item to list with no more room left

---
+++ CustomListProject/CustomList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class CustomList<T>
+    {
+        T[] list;
+        public int count;
+        public int capacity;
+
+        public CustomList()
+        {
+            list = new T[4];
+            count = 0;
+            capacity = 4;
+        }
+
+        public void Indexer()
+        {
+
+        }
+
+        public void AddItem(T item)
+        {
+
+        }
+
+        public void RemoveItem(T item)
+        {
+
+        }
+
+        public void ToString()
+        {
+
+        }
+
+        public void ZipLists()
+        {
+
+        }
+    }
+}
