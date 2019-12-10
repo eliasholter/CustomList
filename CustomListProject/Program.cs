aee0886f6ef50df03b7ff5183667e80cbd566438
Elias Holter <eliasholter1@gmail.com>
2019-12-10 11:19:50 -0600

    Added .txt file for Subtraction Operator documention and cleared program class so it's ready to mess with for instructors

--- CustomListProject/Program.cs
+++ CustomListProject/Program.cs
@@ -10,21 +10,7 @@ namespace CustomListProject
     {
         static void Main(string[] args)
         {
-            CustomList<int> listOne = new CustomList<int>(4);
-            CustomList<int> listTwo = new CustomList<int>(4);
-            CustomList<int> newList;
 
-            listOne.Add(1);
-            listOne.Add(3);
-            listOne.Add(5);
-            listTwo.Add(2);
-            listTwo.Add(3);
-            listTwo.Add(6);
-
-            newList = listOne - listTwo;
-
-            Console.WriteLine(newList);
-            Console.ReadLine();
         }
     }
 }
